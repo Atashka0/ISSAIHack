603873c8d03a49e1853d905a8eb4db8b1762e72c
Ilyas Kudaibergenov <ilyaskb@sajda.com>
2024-11-16 12:58:47 +0500

    first commit

---
+++ ISSAIHack/ISSAIHack/ISSAIHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  ISSAIHackApp.swift
+//  ISSAIHack
+//
+//  Created by Ilyas Kudaibergenov on 16.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ISSAIHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
